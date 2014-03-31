d94fc0b7d9c50b725c52f3e3530fec5060dd1029
Christian Gaser <christian.gaser@uni-jena.de>
2014-03-31 12:57:14 +0000

    Changed paths: M       Makefile A       cg_correct_bias_rats.m M       rSPM.man M       spm_rSPM.m
    Added correction for bias.
    
    
    git-svn-id: svn://141.35.69.214/rSPM@37 345b23fc-aba4-46dc-b74d-347db3efebed

---
+++ cg_correct_bias_rats.m
@@ -0,0 +1,37 @@
+function cg_correct_bias_rats
+
+if nargin==0
+    P = spm_select(Inf,'image','Select images');
+end
+V = spm_vol(P);
+s0 = [6 6 6];
+
+spm_progress_bar('Init',size(P,1),'remove NaN','volumes completed')
+for i = 1:size(P,1)
+	vol = spm_read_vols(V(i));
+	
+    VOX = sqrt(sum(V(i).mat(1:3,1:3).^2));
+    s  = s0./VOX;                        % voxel anisotropy
+    s1 = s/sqrt(8*log(2));              % FWHM -> Gaussian parameter
+
+    x  = round(6*s1(1)); x = -x:x; x = spm_smoothkern(s(1),x,1); x  = x/sum(x);
+    y  = round(6*s1(2)); y = -y:y; y = spm_smoothkern(s(2),y,1); y  = y/sum(y);
+    z  = round(6*s1(3)); z = -z:z; z = spm_smoothkern(s(3),z,1); z  = z/sum(z);
+
+    i2  = (length(x) - 1)/2;
+    j2  = (length(y) - 1)/2;
+    k2  = (length(z) - 1)/2;
+
+    vol2 = zeros(size(vol));
+    spm_conv_vol(vol,vol2,x,y,z,-[i2,j2,k2]);
+
+    vol = vol./(vol2 + eps);
+
+    [pth,nm,xt,vr] = spm_fileparts(V(i).fname);
+    V2 = V(i);
+    V2.fname = fullfile(pth,['m' nm xt vr]);
+    V2.dt(1) = 16;
+    spm_write_vol(V2,vol);
+	spm_progress_bar('Set',i);
+end
+spm_progress_bar('Clear');
